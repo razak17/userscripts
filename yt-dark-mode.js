ead57c00ea6cf9431a054d89f88ef0db6bd29f7d
Razak Mo <moabdulrazak17@gmail.com>
2023-10-16 18:56:26 +0000

    chore: set cookie correctly

--- yt-dark-mode.js
+++ yt-dark-mode.js
@@ -1,21 +1,18 @@
-// ==UserScript==
 // @name         YouTube - Auto Dark Mode
 // @namespace    http://yournamespace.com/
 // @version      0.1
 // @description  Dark Mode for YouTube
 // @author       You
-// @match        https://www.youtube.com/watch?v=*
+// @match       *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
-(function () {
-  "use strict";
-  var c = document.cookie;
+"use strict";
+var c = document.cookie;
 
-  // If there's no PREF cookie, then create one with a value that enables the Dark Theme
-  if (!c || !c.match("^PREF=|; ?PREF=")) {
-    document.cookie = "PREF=f6=400;domain=youtube.com";
-  }
-})();
+// If there's no PREF cookie, then create one with a value that enables the Dark Theme
+if (!c || !c.match("^PREF=|; ?PREF=")) {
+  document.cookie = "PREF=f6=400;domain=youtube.com";
+}
